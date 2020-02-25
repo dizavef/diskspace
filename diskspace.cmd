080a012739853cf83afc95167c52ddd467b6477b
dizavef <56360484+dizavef@users.noreply.github.com>
2020-02-25 17:11:44 -0500

    Update diskspace.cmd

--- diskspace.cmd
+++ diskspace.cmd
@@ -1,9 +1,11 @@
 @echo off
-echo Diskspace usage report > diskspace.txt
-echo .  >> diskspace.txt 
-echo Computer name: %computername% >> diskspace.txt 
+echo ::::::::::::::::::::::: > diskspace.txt
+echo :: Disk usage report :: >> diskspace.txt
+echo ::::::::::::::::::::::: >> diskspace.txt
+echo. >> diskspace.txt
+echo Computer name: %computername% >> diskspace.txt
 echo Report date: %date% >> diskspace.txt
-echo . >> diskspace.txt 
+echo. >> diskspace.txt
 query user >> diskspace.txt
-echo . >> diskspace.txt 
+echo. >> diskspace.txt
 fsutil volume diskfree c: >> diskspace.txt
